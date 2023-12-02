5ecc8764284bc5a5ffee5fc0b1c7309ab68c7854
yogesh <103855728+yogesh4343@users.noreply.github.com>
2023-12-02 23:52:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 require('dotenv').config();
 const connectDb = require('./db/connect');
 const api_router = require('./routes/apiRoutes');
-const cors = require('cors');
 
 
 
 const app = express()
-app.use(cors());
 // var router = express.Router();
 const PORT = process.env.PORT || 4000
 
